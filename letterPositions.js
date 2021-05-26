37522c60437d9203614292546736a1de2a2cedb9
Mingfeng Li <lmf950122@gmail.com>
2021-05-26 19:42:42 +0000

    Find the index of each letter in a string

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`🤗 Assertion of arrays passed: ${arr1} === ${arr2}`);
+    return true;
+  } else {
+    console.log(`😰 Assertion of arrays failed: ${arr1} !== ${arr2}`);
+    return false;
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    let arrInd = [];
+    if (!results[sentence[i]]) {
+      arrInd.push(i);
+      results[sentence[i]] = arrInd;
+    } else {
+      arrInd.push(i);
+      results[sentence[i]] = arrInd;
+    }
+  }
+  //console.log(results);
+  return results;
+};
+
+letterPositions("hello");
+assertArraysEqual(letterPositions("hello").e, [1]);
