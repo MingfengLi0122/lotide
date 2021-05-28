a0b60e2984a7806a2cb8dceb078b649cbe84ea97
Mingfeng Li <lmf950122@gmail.com>
2021-05-28 23:55:34 +0000

    Refactor

--- loopyRefactor.js
+++ loopyRefactor.js
@@ -1,9 +1,9 @@
-function loopyLighthouse(range, multiples, words) {
-  var start = range[0];
-  var end = range[1];
+let loopyLighthouse = function(range, multiples, words) {
+  let start = range[0];
+  let end = range[1];
   while (start <= end) {
-    var temp = start;
-    if(temp % multiples[0] === 0 && temp % multiples[1] !== 0) {
+    let temp = start;
+    if (temp % multiples[0] === 0 && temp % multiples[1] !== 0) {
       console.log(words[0]);
     } else if (temp % multiples[1] === 0 && temp % multiples[0] !== 0) {
       console.log(words[1]);
@@ -14,7 +14,7 @@ function loopyLighthouse(range, multiples, words) {
     }
     start++;
   }
-}
+};
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 
