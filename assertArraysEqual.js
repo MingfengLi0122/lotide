21ce7c97f0158f56a098796bb8fc8f01951fc1b7
Mingfeng Li <lmf950122@gmail.com>
2021-05-31 20:19:07 +0000

    Refactor with mocha and chai

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,3 @@
-
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     return true;
--- test/assertArrayEqual.js
+++ test/assertArrayEqual.js
@@ -4,5 +4,4 @@ const eqArrays = require("../eqArrays");
 
 const arr1 = [2, 3, 4];
 const arr2 = [2, "3", 4];
-//assertArraysEqual(arr1, arr2);
 assertEqual(assertArraysEqual(arr1, arr2), false);
